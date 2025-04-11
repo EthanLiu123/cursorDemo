f2f558748e66926637c84ca0b7d4dc73de581939
liuchenxu <maidou3302@163.com>
2025-04-11 21:10:38 +0800

    新增代码分发

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from utils import distribute_code
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,8 +7,9 @@ def index():
     output = ""
     if request.method == 'POST':
         input_text = request.form.get('input_text', '')
-        output = f"您输入的内容是: {input_text}"
+        output =distribute_code(input_text)
     return render_template('index.html', output=output)
 
 if __name__ == '__main__':
+    print("tets")
     app.run(debug=True) 
---
+++ utils.py
@@ -0,0 +1,2 @@
+def distribute_code(code:str)->str:
+    return f"这是我的代码解析模块:{code}"
